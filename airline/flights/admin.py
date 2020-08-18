8ed3be717631be898e914d21c1aa7078041ffc71
yidaaa <59677662+yidaaa@users.noreply.github.com>
2020-08-19 00:02:45 +0800

    lecture 4 complete

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Flight, Airport
+from .models import Flight, Airport, Passenger
 # Register your models here.
 
-# can be opened by /admin
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("origin", "destination", "duration", "id")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
+# can be opened by url: /admin
 # tells django you can use admin to manipulate flight and airport
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight, Passenger, Airport
 
 # Create your views here.
 def index(request):
@@ -11,4 +13,14 @@ def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all(),
     })
+
+def book(request, flight_id):
+    if request.method =="POST":
+        flight = Flight.objects.get(pk=flight_id)
+        # name of input POST form is passenger
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
